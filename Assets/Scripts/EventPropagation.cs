ed6cbe4b5193f1898176aa343c2d09eb92b1bedf
cathode26 <Vincent.J.Liguori@outlook.com>
2023-07-26 12:08:32 -0400

    Event propagation emphasizing trickle down and stop propagation

--- Assets/Scripts/EventPropagation.cs
+++ Assets/Scripts/EventPropagation.cs
@@ -3,11 +3,31 @@ using UnityEngine.UIElements;
 
 public class EventPropagation : MonoBehaviour
 {
+    VisualElement root;
     private void OnEnable()
     {
-        VisualElement root = GetComponent<UIDocument>().rootVisualElement;
+        root = GetComponent<UIDocument>().rootVisualElement;
+        //DetermineClickWithoutEvent();
+        PropagationTest();
+    }
 
+    private void DetermineClickWithoutEvent()
+    {
         VisualElement blue = root.Q("Blue");
+        blue.RegisterCallback<MouseDownEvent>(evt =>
+        {
+            Debug.Log((evt.target as VisualElement).name + " was clicked");
+            Debug.Log(evt.propagationPhase);
+            ColorUtility.TryParseHtmlString("#F22F46", out Color defaultColor);
+            (evt.target as VisualElement).style.backgroundColor = defaultColor;
+        });
+    }
+
+    private void PropagationTest()
+    {
+        VisualElement blue = root.Q("Blue");
+        blue.RegisterCallback<MouseDownEvent>(evt => Debug.Log("blue " + evt.propagationPhase));
+
         VisualElement yellow = root.Q("Yellow");
         VisualElement orange = root.Q("Orange");
         VisualElement black = root.Q("Black");
@@ -16,11 +36,26 @@ public class EventPropagation : MonoBehaviour
         VisualElement red = root.Q("Red");
         VisualElement grey = root.Q("Grey");
 
-        blue.RegisterCallback<MouseDownEvent>(evt => Debug.Log("blue " + evt.propagationPhase));
+        yellow.RegisterCallback<MouseDownEvent>(evt =>
+        {
+            Debug.Log("yellow " + evt.propagationPhase);
+            evt.StopPropagation();
+        });//, 
+        //TrickleDown.TrickleDown);
 
-        yellow.RegisterCallback<MouseDownEvent>(evt => Debug.Log("yellow " + evt.propagationPhase), TrickleDown.TrickleDown);
-        orange.RegisterCallback<MouseDownEvent>(evt => Debug.Log("orange " + evt.propagationPhase), TrickleDown.TrickleDown);
-        black.RegisterCallback<MouseDownEvent>(evt => Debug.Log("black " + evt.propagationPhase), TrickleDown.TrickleDown);
+        orange.RegisterCallback<MouseDownEvent>(evt =>
+        {
+            Debug.Log("orange " + evt.propagationPhase);
+            evt.StopPropagation();
+        });//,
+           //TrickleDown.TrickleDown);
+
+        black.RegisterCallback<MouseDownEvent>(evt =>
+        {
+            Debug.Log("black " + evt.propagationPhase);
+            evt.StopPropagation();
+        });//, 
+        //TrickleDown.TrickleDown);
 
         green.RegisterCallback<MouseDownEvent>(evt => Debug.Log("green " + evt.propagationPhase), TrickleDown.TrickleDown);
         red.RegisterCallback<MouseDownEvent>(evt => Debug.Log("red " + evt.propagationPhase), TrickleDown.TrickleDown);
